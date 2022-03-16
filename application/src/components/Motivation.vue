42b43ed269d8e3849fef8e066aa13f4c85aef067
Joji_Sakata <helloworld01joji25@gmail.com>
2022-03-17 00:07:51 +0900

    簡素なアプリ作成

---
+++ application/src/components/Motivation.vue
@@ -0,0 +1,108 @@
+<template>
+  <v-container>
+    <v-row class="text-center mt-3">
+      <v-col>
+        <v-row class="justify-center">
+          ニックネームと回数を入力してね。
+        </v-row>
+        <v-row dense class="justify-center">
+          <v-col cols="8" >
+            <v-text-field
+              label="ニックネーム"
+              v-model="nickname"
+              placeholder="ニックネームを入力"
+              :rules="[required]"
+            ></v-text-field>
+          </v-col>
+          <v-col cols="4">
+            <v-text-field
+              label="回数"
+              v-model="count"
+              placeholder="回数を入力"
+              maxlength="3"
+              suffix="回"
+              :rules="[required]"
+            ></v-text-field>
+          </v-col>
+        </v-row>
+        <v-row dense>
+          <v-col
+            class="d-flex"
+            cols="12"
+            sm="6"
+          >
+            <v-select
+              v-model="message"
+              :items="items"
+              label="メッセージ何がいい？"
+              dense
+              :hint="`メッセージ内容+ニックネーム`"
+              persistent-hint
+            ></v-select>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col>
+            <v-dialog
+              transition="dialog-bottom-transition"
+              max-width="600"
+            >
+              <template v-slot:activator="{ on, attrs }">
+                <v-row class="justify-center">
+                  <v-btn
+                    color="primary"
+                    v-bind="attrs"
+                    v-on="on"
+                  >実行</v-btn>
+                </v-row>
+              </template>
+              <template v-slot:default="dialog">
+                <v-card>
+                  <v-toolbar
+                    color="primary"
+                    dark
+                  >元気！？</v-toolbar>
+                  <v-card-text class="mt-2">
+                    <ul>
+                     <li v-for="i in Number(count)" :key="i">{{message}}{{nickname}}！！</li>
+                    </ul>
+                  </v-card-text>
+                  <v-card-actions class="justify-end">
+                    <v-btn
+                      text
+                      @click="dialog.value = false"
+                    >閉じる</v-btn>
+                  </v-card-actions>
+                </v-card>
+              </template>
+            </v-dialog>
+          </v-col>
+        </v-row>
+        <v-row>
+
+        </v-row>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    name: 'HelloWorld',
+    data(){
+      return{
+        count:null,
+        message: null,
+        nickname: null,
+        button:false,
+        required: value => !!value || '必ず入力してね。',
+        items: ["頑張れ！！", "元気！？", "そういう日もあるよね、", "こっからっしょ！"]
+      }
+    },
+    methods:{
+      enter(){
+        this.button = true;
+      },
+    }
+  }
+</script>
